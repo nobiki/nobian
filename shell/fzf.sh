a92fc6f8af1dff45347ca95aea6bcdfa7163c4bc
Naoaki Obiki <obiki.wk@gmail.com>
2020-02-02 22:51:43 +0900

    not working

--- shell/fzf.sh
+++ shell/fzf.sh
@@ -1,8 +1,6 @@
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --key-bindings --completion --update-rc
 
-echo '[ -f ~/.fzf.bash ] && source ~/.fzf.bash' >> ~/.bash_profile
-
 cat << 'EOS' > ~/.fzf.bash
 # Setup fzf
 # ---------
