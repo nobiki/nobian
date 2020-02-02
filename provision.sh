bb03e2790efe4a64488574e155c791b5bd71c572
Naoaki Obiki <obiki.wk@gmail.com>
2020-02-02 23:06:30 +0900

    logo

--- provision.sh
+++ provision.sh
@@ -2,6 +2,15 @@
 export DEBIAN_FRONTEND=noninteractive
 export HOSTNAME=nobian
 
+cat << 'EOS'
+                __    _
+   ____  ____  / /_  (_)___ _____
+  / __ \/ __ \/ __ \/ / __ `/ __ \
+ / / / / /_/ / /_/ / / /_/ / / / /
+/_/ /_/\____/_.___/_/\__,_/_/ /_/
+
+EOS
+
 ## Set Hostname
 hostnamectl set-hostname ${HOSTNAME}
 hostnamectl
